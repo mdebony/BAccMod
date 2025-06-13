19125709d2110b0271008ab59dcea889a6a087fd
Mathieu de Bony <debony@cppm.in2p3.fr>
2025-06-13 14:52:45 +0200

    Fix merge issues

--- test/integration_test.py
+++ test/integration_test.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 import astropy.units as u
@@ -60,16 +61,17 @@ class TestIntegrationClass:
                                  rtol=self.relative_tolerance))
 
     def test_integration_spatial_fit(self):
-        bkg_maker = Grid3DAcceptanceMapCreator(energy_axis=self.energy_axis,
-                                               offset_axis=self.offset_axis,
-                                               oversample_map=5,
-                                               exclude_regions=self.exclude_region_PKS_2155,
-                                               method='fit')
+        bkg_maker = SpatialFitAcceptanceMapCreator(energy_axis=self.energy_axis,
+                                                   offset_axis=self.offset_axis,
+                                                   oversample_map=5,
+                                                   exclude_regions=self.exclude_region_PKS_2155)
         background_model = bkg_maker.create_acceptance_map(observations=self.obs_collection_pks_2155)
         assert type(background_model) is Background3D
+
         reference = Background3D.read('ressource/test_data/reference_model/pks_2155_spatial_fit_bkg.fits')
-        for i in range(background_model.data.shape[0]):
-            print(np.sum((np.abs(background_model.data[i, : ,:] - reference.data[i, : ,:]) / reference.data[i, : ,:]) > 1e-3))
+        relative_error = np.abs(background_model.data - reference.data) / reference.data
+        if np.sum(relative_error > 1e-4) > 0:
+            logging.warning(f'Maximum relative error : {np.nanmax(relative_error)}, fraction above 1e-3 : {np.sum(relative_error > 1e-3)/relative_error.size}, fraction above 1e-2 : {np.sum(relative_error > 1e-2)/relative_error.size}, fraction above 1e-1 : {np.sum(relative_error > 1e-1)/relative_error.size}')
         assert np.all(np.isclose(background_model.data, reference.data,
                                  atol=self.absolute_tolerance,
                                  rtol=self.relative_tolerance))
@@ -85,14 +87,6 @@ class TestIntegrationClass:
         assert np.all(np.isclose(background_model.data, reference.data,
                                  atol=self.absolute_tolerance,
                                  rtol=self.relative_tolerance))
-
-    def test_integration_spatial_fit(self):
-        bkg_maker = SpatialFitAcceptanceMapCreator(energy_axis=self.energy_axis,
-                                                   offset_axis=self.offset_axis,
-                                                   oversample_map=5,
-                                                   exclude_regions=self.exclude_region_PKS_2155)
-        background_model = bkg_maker.create_acceptance_map(observations=self.obs_collection_pks_2155)
-        assert type(background_model) is Background3D
 
     def test_integration_zenith_binned_model(self):
         bkg_maker = RadialAcceptanceMapCreator(energy_axis=self.energy_axis,
