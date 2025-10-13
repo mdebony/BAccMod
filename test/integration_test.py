f93128ba6bbe1ae53290cfe48472b4831b165bdc
Gabriel Emery <gabriel-emery@hotmail.fr>
2025-10-13 15:58:17 +0200

    Update tests

--- test/integration_test.py
+++ test/integration_test.py
@@ -151,7 +151,7 @@ class TestIntegrationClass:
                                                dynamic_energy_axis_target_statistics=100,
                                                oversample_map=5,
                                                exclude_regions=self.exclude_region_PKS_2155)
-        background_model = bkg_maker.create_acceptance_map(observations=self.obs_collection_pks_2155)
+        background_model = bkg_maker.create_model(observations=self.obs_collection_pks_2155)
         assert type(background_model) is Background3D
         reference = Background3D.read('ressource/test_data/reference_model/pks_2155_3D_bkg_dynamic_irregular_energy.fits')
         self._print_model_precision(background_model, reference)
@@ -168,7 +168,7 @@ class TestIntegrationClass:
                                                oversample_map=5,
                                                exclude_regions=self.exclude_region_PKS_2155,
                                                method='fit')
-        background_model = bkg_maker.create_acceptance_map(observations=self.obs_collection_pks_2155)
+        background_model = bkg_maker.create_model(observations=self.obs_collection_pks_2155)
         assert type(background_model) is Background3D
         reference = Background3D.read('ressource/test_data/reference_model/pks_2155_spatial_fit_bkg_dynamic_irregular_energy.fits')
         self._print_model_precision(background_model, reference)
@@ -184,7 +184,7 @@ class TestIntegrationClass:
                                                dynamic_energy_axis_target_statistics=500,
                                                oversample_map=5,
                                                exclude_regions=self.exclude_region_PKS_2155)
-        background_model = bkg_maker.create_acceptance_map(observations=self.obs_collection_pks_2155)
+        background_model = bkg_maker.create_model(observations=self.obs_collection_pks_2155)
         assert type(background_model) is Background2D
         reference = Background2D.read('ressource/test_data/reference_model/pks_2155_2D_bkg_dynamic_irregular_energy.fits')
         self._print_model_precision(background_model, reference)
