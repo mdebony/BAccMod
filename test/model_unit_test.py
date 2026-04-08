3bf7eb9bef6dc09e7a827665312e69a117f02b21
Gabriel Emery <gabriel-emery@hotmail.fr>
2026-04-08 13:57:32 +0200

    Add value checks in unit tests

--- test/model_unit_test.py
+++ test/model_unit_test.py
@@ -9,20 +9,36 @@ from baccmod.model import (CustomModels,
                            PLCutOffsEnergyxGaussGradSpatial)
 
 
+
 class TestModelClass(unittest.TestCase):
+
+    numerical_r_tolerance = 1e-5
+    numerical_a_tolerance = 1e-8
 
     def test_powerlaw_cutoffs(self):
         model = PowerLawCutOffsEnergy()
-        model.evaluate(1, amplitude=100.0, index=2.2, eref=1.0, b=2.1, ecl=0.1, ech=100.0)
-        model.evaluate(np.array([1, 2, 3, 4, 5]), amplitude=100.0, index=2.2, eref=1.0, b=2.1, ecl=0.1, ech=100.0)
+        e1 = model.evaluate(1, amplitude=100.0, index=2.2, eref=1.0, b=2.1, ecl=0.1, ech=100.0)
+        e2 = model.evaluate(np.array([1, 2, 3, 4, 5]), amplitude=100.0, index=2.2, eref=1.0, b=2.1, ecl=0.1, ech=100.0)
+        assert np.isclose(e1, 98.22167398064686,
+                          rtol=self.numerical_r_tolerance,
+                          atol=self.numerical_a_tolerance)
+        assert np.all(np.isclose(e2, [98.22167398, 21.29332307,  8.64890211,  4.54892255,  2.75698111],
+                                 rtol=self.numerical_r_tolerance,
+                                 atol=self.numerical_a_tolerance))
 
     def test_2dmodel_gaussgrad(self):
         model = Gaussian2DLinearGradientsSpatial()
-        model.evaluate(x=0, y=2)
+        e = model.evaluate(x=0, y=2)
+        assert np.isclose(e, 0.1353352832366127,
+                          rtol=self.numerical_r_tolerance,
+                          atol=self.numerical_a_tolerance)
 
     def test_3dmodel_plcuts_gaussgrad(self):
         model = PLCutOffsEnergyxGaussGradSpatial()
-        model.evaluate(e=5, x=0, y=2)
+        e = model.evaluate(e=5, x=0, y=2)
+        assert np.isclose(e, 0.005147336796951535,
+                          rtol=self.numerical_r_tolerance,
+                          atol=self.numerical_a_tolerance)
 
     def test_CustomModels_specialcases(self):
         with self.assertRaises(NotImplementedError):
